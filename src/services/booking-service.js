c542963e547444295a23f30fffc22ac1da95c0d5
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-07-16 09:59:00 +0530

    added code

--- src/services/booking-service.js
+++ src/services/booking-service.js
@@ -16,26 +16,22 @@ const bookingRepository = new BookingRepository();
 const FLIGHT_SERVICE_URL = `${ServerConfig.FLIGHT_SERVICE_URL}/api/v1`;
 
 async function createBooking(data) {
-    console.log('Input data:', data); // Log input data
-    if (!data || !data.flightId) {
-        throw new Error('Invalid data provided to createBooking');
-    }
+    //un managed transaction
     const transaction = await db.sequelize.transaction();
     try {
         const uri = FLIGHT_SERVICE_URL + `/flights/${data.flightId}`;
         const flight = await axios.get(uri);
         const flightData = flight.data.SuccessResponse.data;
-        console.log(flightData);
+        // console.log(flightData);
         if(data.noofSeats > flightData.totalSeats){
             throw new AppError("Not enough seats available",StatusCodes.BAD_REQUEST);
         }
+        await transaction.commit();
         return true;
-        // console.log(flight.totalSeats);
-        // // const flightData = flight.data;
-        // // console.log(flightData);
+      
     } catch (error) {
         console.log(error);
-        //await transaction.rollback();
+        await transaction.rollback();
         throw error;
     }
 }
