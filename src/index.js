7bcc16ba9ce5ad4b434d1230ee62c1727a69d6af
srikrishnastrive <srikrishnamuddangala@gmail.com>
2024-07-25 16:50:38 +0530

    added the publisher rabbit mq

--- src/index.js
+++ src/index.js
@@ -1,4 +1,20 @@
 const express = require('express');
+const amqplib = require('amqplib');
+
+async function connectQueue(){
+    try {
+        const connection = await amqplib.connect('amqp://localhost');
+        const channel = await connection.createChannel();
+        await channel.assertQueue('noti-queue');
+        setInterval(()=>{
+            channel.sendToQueue("noti-queue",Buffer.from("this is krishna"));
+        },1000);
+       
+
+    } catch (error) {
+        console.log(error);
+    }
+}
 
 const { ServerConfig } = require('./config');
 const apiRoutes = require('./routes');
@@ -13,7 +29,9 @@ app.use(express.urlencoded({extended: true}));
 app.use('/api', apiRoutes);
 app.use('/bookingService/api',apiRoutes);
 
-app.listen(ServerConfig.PORT, () => {
+app.listen(ServerConfig.PORT, async () => {
     console.log(`Successfully started the server on PORT : ${ServerConfig.PORT}`);
     CRON();
+    await connectQueue();
+    console.log('queue is up');
 });
